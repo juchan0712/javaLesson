a4238b20e2c515dfb2f48a9c56c9a4e1ef50abfd
kangjuchan <wncks2536@gmail.com>
2024-01-05 19:19:25 +0900

    숙제

--- partB/src/object/day6/AddQuiz.java
+++ partB/src/object/day6/AddQuiz.java
@@ -1,5 +1,34 @@
 package object.day6;
 
 public class AddQuiz {
+    private int data1;
+    private int data2;
+    private int userAnswer;
+
+	public int getData1() {
+		return this.data1;
+	}
+
+	public void setData1(int data1) {
+		this.data1 = data1;
+	}
+
+	public int getData2() {
+		return this.data2;
+	}
+
+	public void setData2(int data2) {
+		this.data2 = data2;
+	}
+
+	public int getUserAnswer() {
+		return this.userAnswer;
+	}
+
+	public void setUserAnswer(int userAnswer) {
+		this.userAnswer = userAnswer;
+	}
+
+
     
 }
