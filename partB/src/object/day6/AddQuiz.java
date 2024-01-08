695826e0f068953503ff5d331ed72dc66f1b4c2c
kangjuchan <wncks2536@gmail.com>
2024-01-08 17:18:20 +0900

    day6 최종

--- partB/src/object/day6/AddQuiz.java
+++ partB/src/object/day6/AddQuiz.java
@@ -3,32 +3,50 @@ package object.day6;
 public class AddQuiz {
     private int data1;
     private int data2;
+    private String question;
     private int userAnswer;
+    private int answer;
 
-	public int getData1() {
-		return this.data1;
-	}
+    public int getData1(){
+        return this.data1;
+    }
 
-	public void setData1(int data1) {
-		this.data1 = data1;
-	}
+    public int getData2(){
+        return this.data2;
+    }
+    
+    public String getQuestion() {
+        return question;
+    }
 
-	public int getData2() {
-		return this.data2;
-	}
+    public int getUserAnswer() {
+        return userAnswer;
+    }
 
-	public void setData2(int data2) {
-		this.data2 = data2;
-	}
+    public int getAnswer() {
+        return answer;
+    }
 
-	public int getUserAnswer() {
-		return this.userAnswer;
-	}
+    public AddQuiz(String question, int answer) {
+        this.question = question;
+        this.answer = answer;
+    }
 
-	public void setUserAnswer(int userAnswer) {
-		this.userAnswer = userAnswer;
-	}
+    public void setData1(int data1){
+        this.data1 = data1;
+    }
+    
+    public void setData2(int data2){
+        this.data2 = data2;
+    }
 
+    public void setUserAnswer(int userAnswer) {
+        this.userAnswer = userAnswer;
+    }
+
+    public boolean isCorrect() {
+        return userAnswer == answer;
+    }
 
     
 }
--- partB/src/object/day6/AddQuizMain.java
+++ partB/src/object/day6/AddQuizMain.java
@@ -1,39 +1,53 @@
 package object.day6;
 
-public class AddQuizMain {
-    public static void main(String[] args) {
-        // 숙제할 때 테스트하고 사용해야할 코드
-        /*
-         * System.out.println(" 난수(컴퓨터가 만들어주는 임의 값) - random");
-         * System.out.println(" 난수값을 만들어주는 방법은 Math.random()");
-         * System.out.println(" 0~1 사이의 double 실수를 무작위로 만들어줍니다.");
-         * System.out.println("Math.random() = " + Math.random());
-         * System.out.println("Math.random() = " + Math.random());
-         * System.out.println("Math.random() = " + Math.random());
-         * System.out.println("Math.random() = " + Math.random());
-         * System.out.println("Math.random() = " + Math.random());
-         * System.out.println("Math.random() = " + Math.random());
-         * System.out.println("Math.random() = " + Math.random());
-         * // 0 < 난수 < 1 => 로또 값처럼 1 <= 로또번호 <= 45
-         * // 원래 난수값 범위에 *45를 하고 0 < 난수*45 < 45
-         * // 그 값에 다시 +1 하면 됩니다. 1 < 난수*45+1 < 46
-         * // 난수의 범위를 start ~ end 범위로 조정하려면
-         * // (int)(Math.random()*(end-start+1))+start
-         * int start = 1;
-         * int end = 45;
-         * for (int i = 0; i < 6; i++) {
-         * int temp = (int) (Math.random() * (end - start + 1)) + start;
-         * System.out.println("1 ~ 45 범위의 난수 : " + temp);
-         * }
-         */
+import java.util.Scanner;
 
+public class AddquizMain {
+    public static void main(String[] args) {
+        
         int start = 11;
         int end = 99;
-        for (int i = 0; i < 99; i++) {
-            int temp1 = (int) (Math.random() * (end - start + 1)) + start;
-            System.out.println("11 ~ 99 범위의 난수 : " + temp1);
-        }
+        int quizCount = 3;
+
+        AddQuiz[] quizz = new AddQuiz[quizCount];
         
 
+        Scanner scanner = new Scanner(System.in);
+
+        for (int i = 0; i < quizCount; i++) {
+            AddQuiz quiz = new AddQuiz(null, i);
+            int data1 = (int) (Math.random() * (end - start + 1)) + start;
+            quiz.setData1(data1);
+            int data2 = (int) (Math.random() * (end - start + 1)) + start;
+            quiz.setData2(data2);
+            int answer = data1 + data2;
+
+            String question = (i + 1) + "번. " + quiz.getData1() + "+" + quiz.getData2() + "=?";
+            quizz[i] = new AddQuiz(question, answer);
+        }
+
+        for (AddQuiz quiz : quizz) {
+            System.out.println(quiz.getQuestion());
+            System.out.print("답을 입력하세요 >>> ");
+            int userAnswer = scanner.nextInt();
+            quiz.setUserAnswer(userAnswer);
+        }
+
+        System.out.println(":::::: 채점 하고 있습니다. ::::::");
+        System.out.println("            문제             제출한답              정답         채점");
+        int Count = 0;
+        for (AddQuiz quiz : quizz) {
+            System.out.printf("%17s\t %10d\t %14d\t %10s\n",
+                    quiz.getQuestion(), quiz.getUserAnswer(), quiz.getAnswer(),
+                    quiz.isCorrect() ? "O" : "X");
+            if (quiz.isCorrect()) {
+                Count++;
+            }
+        }
+        System.out.println("::: 맞은 갯수 " + Count + " 입니다. :::");
     }
 }
+
+
+
+
